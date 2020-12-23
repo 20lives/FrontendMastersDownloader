cc5c173bfcccc739118dc6d8b8e53be059dee8a6
Ali Shaikh <Ali-Shaikh@users.noreply.github.com>
2020-12-23 22:02:09 +0500

    Updated utils.js - Enhanced sanitization
    # String
    "Some Name (ft. Apple, Banana & Mango 56)"
    "Test string, this is,"
    # Before
    "Some Name -ft. Apple- Banana - Mango 56-"
    "Test string- this is-"
    # After
    "Some Name ft. Apple - Banana - Mango 56"
    "Test string - this is"

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,1 @@
-export const sanitize = str => str.replace(/[^\w\s-._]/gi, '-');
+export const sanitize = str => str.replace(/\b\)|\(\b|(,$)/gi, '').replace(/\b,/gi, ' -').replace(/[^\w\s-._]/gi, '-');
